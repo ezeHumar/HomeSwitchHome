181ce1d6f97e0142b6db7e97c1e3304a0a4a4466
Emanuelm26 <emanuelm26@gmail.com>
2018-11-09 09:32:45 -0300

    Agregados metodos en controlador de Residence

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -1,2 +1,8 @@
 class ResidencesController < ApplicationController
+  def index
+    @residences = Residence.all
+  end
+  def new
+    @residences = Residence.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :auctions
-  resources :residences
+  resources :residences # index create edit update destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
