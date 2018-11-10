1552715affbe5cd55594eb86740a3a9c88a6110b
Ezequiel Humar <humarezequiel@gmail.com>
2018-11-10 18:14:38 -0300

    Implementacion para eliminar

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -13,14 +13,22 @@ class ResidencesController < ApplicationController
       render :new
     end
   end
+
   def destroy
-    @residence = Residence.destroy
+    @residence=Residence.find(params[:id])
+
+    if @residence.destroy
+      redirect_to residence_path, notice:"La residencia '#{@residence.name}' ha sido eliminada exitosamente"
+    else
+      redirect_to residences_path, notice:"No se ha podido eliminar la residencia '#{@residence.name}'"
+    end
   end
-  
+
   def edit
     # params = { id: 7}
     @residence = Residence.find(params[:id])
   end
+
 
   def update
     @residence = Residence.find(params[:id])
