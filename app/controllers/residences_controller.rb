b605061a6415cc09e9aa6c6855ce0d14227daea2
Emanuelm26 <emanuelm26@gmail.com>
2018-11-11 23:21:33 -0300

    Correcciones de Controller de Residencias

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -9,7 +9,7 @@ class ResidencesController < ApplicationController
     @residence = Residence.new
   end
   def create
-    @residence = Residence.new(params.require(:residence).permit(:name, :location, :capacity, :photos))
+    @residence = Residence.new(residence_params))
     if @residence.save
       redirect_to residences_path, notice: "Residencia cargada correctamente"
     else
