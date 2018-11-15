d3aa93b2dec3bd5669ebb932f151a7d7e756352c
Ezequiel Humar <humarezequiel@gmail.com>
2018-11-14 23:12:15 -0300

    Arregladas migraciones

--- db/migrate/20181109121812_create_auctions.rb
+++ db/migrate/20181109121812_create_auctions.rb
@@ -1,7 +1,7 @@
 class CreateAuctions < ActiveRecord::Migration[5.2]
   def change
     create_table :auctions do |t|
-      t.date :strartDate
+      t.date :startDate
       t.integer :residence_id
       t.string :email
       t.float :amount
