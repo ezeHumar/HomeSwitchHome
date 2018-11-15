11d72f1285cceec2da7969997e220c4293806130
Ezequiel Humar <humarezequiel@gmail.com>
2018-11-15 19:14:39 -0300

    Validaciones email y amount

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,4 +1,5 @@
 class Auction < ApplicationRecord
   belongs_to :residence
-  validates :amount, numericality: { greater_than: :amount_was }
+  validates :amount, allow_nil: true, numericality: { greater_than: :amount_was }
+  validates_format_of :email, allow_nil: true,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
