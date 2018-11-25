e9a774dc8882930a6b9db1f5dd904218fabb1115
Ezequiel Humar <humarezequiel@gmail.com>
2018-11-25 20:35:18 -0300

    Solucionado database

--- db/migrate/20181121234400_remove_admin_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAdminToUsers < ActiveRecord::Migration[5.2]
-  def self.up
-  remove_column :users, :admin
-end
-end
--- db/migrate/20181122223505_delete_birth_date.rb
+++
@@ -1,5 +0,0 @@
-class DeleteBirthDate < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :birth_date
-  end
-end
