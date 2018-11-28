0ac61eec607ad09247eb3259b1ea73695d33e964
Ezequiel Humar <humarezequiel@gmail.com>
2018-11-28 19:44:07 -0300

    validaciones residencias

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -4,4 +4,8 @@ class Residence < ApplicationRecord
   validates :capacity, numericality: { only_integer: true, greater_than: 0 }
   validates :name, presence: true
   validates :capacity, presence: true
+  validates :country, presence: true
+  validates :city, presence: true
+  validates :zip, presence: true
+  validates :address, presence: true
 end
