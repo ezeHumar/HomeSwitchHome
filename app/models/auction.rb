c8838b05722f91cedc17642494359c0331fb7337
Ezequiel Humar <humarezequiel@gmail.com>
2018-11-29 12:36:23 -0300

    Fechas auction

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -6,13 +6,14 @@ class Auction < ApplicationRecord
 
   validates :amount, numericality: { greater_than: :amount_was }, if: :persisted?
   validates :residence_id, presence: true
+  validates :startDate, presence: true
   validate :date_mayor_que_hoy
   validates_format_of :email, allow_nil: true,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
   private
 
   def date_mayor_que_hoy
-    if startDate < Date.today
+    if startDate.nil? || startDate < Date.today
       errors.add(:startDate, "Tiene que ser mayor a seis meses desde hoy")
     end
   end
