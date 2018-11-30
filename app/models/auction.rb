c189c0e06b1f324a37e9392ac3c2d6d3d5690def
Ezequiel Humar <humarezequiel@gmail.com>
2018-11-29 23:01:27 -0300

    Cosas diferentes p. usuarios

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -7,10 +7,24 @@ class Auction < ApplicationRecord
   validates :amount, numericality: { greater_than: :amount_was }, if: :persisted?
   validates :residence_id, presence: true
   validates :startDate, presence: true
-  validate :date_mayor_que_hoy
+  #validate :date_mayor_que_hoy
+  #validate :fecha_no_repetida
   validates_format_of :email, allow_nil: true,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
   private
+
+  def fecha_no_repetida
+    rep=false
+    @auctions.each do |a|
+      if a.startDate == startDate
+        rep=true
+        break
+      end
+    end
+    if rep?
+        errors.add(:startDate, "La fecha ingresada ya esta asignada a una subasta de esta residencia")
+    end
+  end
 
   def date_mayor_que_hoy
     if startDate.nil? || startDate < Date.today
