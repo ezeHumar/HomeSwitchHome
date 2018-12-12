be110dc332b93befcf0ffe695025a942b4c9bcec
Emanuelm26 <emanuelm26@gmail.com>
2018-12-12 13:52:08 -0300

    asdas

--- db/schema.rb
+++ db/schema.rb
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2018_12_05_183512) do
     t.date "birth_date"
     t.boolean "admin", default: false
     t.string "credit_card"
+    t.integer "credit"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
