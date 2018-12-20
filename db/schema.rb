363243f37bf6e9be1a998edb51c837884e2dd23e
Ezequiel Humar <humarezequiel@gmail.com>
2018-12-20 20:02:18 -0300

    Dar creditos

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2018_12_20_214947) do
-=======
-ActiveRecord::Schema.define(version: 2018_12_19_002711) do
->>>>>>> 217b2d6621ef0c270beede637d9cdb6fcc630ec8
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
